b1c9ae69c2604fe31ad449473760353b3dcdba0d
orlrod5 <157768122+orlrod5@users.noreply.github.com>
2024-04-10 21:37:04 -0600

    component home

---
+++ front-end/src/components/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { HomeWrapper } from './home.styled';
+
+const Home = () => (
+ <HomeWrapper data-testid="Home">
+    Home Component
+ </HomeWrapper>
+);
+
+Home.propTypes = {};
+
+Home.defaultProps = {};
+
+export default Home;
